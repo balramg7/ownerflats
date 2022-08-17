a0bcc6877c89feadb9d8d265143f642024335d1f
codingblaze - assignment <kartikeyb08@gail.com>
2022-08-17 22:38:59 +0530

    texting

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
-import './App.css';
-import Navbar from './component/Navbar';
-import Card from './component/Card';
-import Table from './component/Table';
+import "./App.css";
+import Navbar from "./component/Navbar";
+import Card from "./component/Card";
+import Table from "./component/Table";
 
 function App() {
-  return (
-    <>
-    <Navbar/>
-    <div className="container my-3 mx-2">
-      <Card/>
-    </div>
-    <div className="container">
-      <Table/>
-    </div>
-    </>
-  );
+    return (
+        <>
+            <Navbar />
+            <div className="container my-3 mx-2">
+                <Card />
+            </div>
+            <div className="container">
+                abc
+                <Table />
+            </div>
+        </>
+    );
 }
 
 export default App;
