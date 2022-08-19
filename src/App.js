89c120e89aced79f36a0ab9507a4d0de8fe97842
codingblaze - assignment <kartikeyb08@gail.com>
2022-08-19 11:41:23 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import "./App.css";
-// import Card from "./component/Card";
-// import Table from "./component/Table";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./component/Header";
 import Dashboard from "./component/content/Dashboard";
@@ -11,24 +9,15 @@ import Info from "./component/content/FlatDetails/Info";
 function App() {
     return (
         <>
-             
             <BrowserRouter>
-            <Header />
-           <Routes>
-           <Route path="/login" element={<Login/>}/>
-           <Route path="/signup" element={<Signup/>}/>
-            <Route path="/dashboard" element={<Dashboard/>}/> 
-            <Route path="/info" element={<Info/>}/>
-           </Routes>
-            </BrowserRouter> 
-            {/* <div className="container my-3 mx-2">
-                <Card />
-            </div>
-            <div className="container">
-                <Table />
-            </div>  */}
-            {/* <Header/> */}
-            
+                <Header />
+                <Routes>
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/signup" element={<Signup />} />
+                    <Route path="/dashboard" element={<Dashboard />} />
+                    <Route path="/info" element={<Info />} />
+                </Routes>
+            </BrowserRouter>
         </>
     );
 }
